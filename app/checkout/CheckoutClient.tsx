493ac7ede852e5bf77ccf4e44914864eedf9781f
Eweh Victor <ewehvictor7@gmail.com>
2024-06-16 20:42:39 +0100

    fixed deployment issue

--- app/checkout/CheckoutClient.tsx
+++ app/checkout/CheckoutClient.tsx
@@ -2,7 +2,7 @@
 
 import { useCart } from "@/hooks/useCart";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import Heading from "../components/Heading";
 import { IoCardSharp } from "react-icons/io5";
@@ -177,113 +177,115 @@ const CheckoutClient = () => {
 
   const isLoading = false;
 
-  return loading ? (
-    <DottedLoadingSpinner />
-  ) : (
-    <form /* onSubmit={handleSubmit(onSubmit)} */ className="relative">
-      <div className="flex flex-col">
-        <div className="mx-auto ">
-          <Heading title="Checkout" />
-        </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-12 gap-8 w-full">
-          <div className="col-span-1 md:col-span-7 w-full">
-            <div className="shadow-xl shadow-slate-200 rounded-md p-4 flex flex-col space-y-2 ">
-              <div className="">
-                <p className="text-lg font-semibold text-slate-600">
-                  Delivery Details
-                </p>
-              </div>
-              <hr />
-              <p className="text-sm  text-red-500">
-                Delivery is within Nigeria *
-              </p>
-
-              <Input
-                id="streetAddress"
-                label="Street Address"
-                disabled={isLoading}
-                register={register}
-                errors={errors}
-                required
-              />
-              <Input
-                id="city"
-                label="City / Town"
-                disabled={isLoading}
-                register={register}
-                errors={errors}
-                required
-              />
-              <Input
-                id="state"
-                label="State"
-                disabled={isLoading}
-                register={register}
-                errors={errors}
-                required
-              />
-              <Input
-                id="phoneNumber"
-                label="Phone Number"
-                disabled={isLoading}
-                register={register}
-                errors={errors}
-                required
-              />
+  return (
+    <Suspense fallback={<DottedLoadingSpinner />}>
+      {loading ? (
+        <DottedLoadingSpinner />
+      ) : (
+        <form /* onSubmit={handleSubmit(onSubmit)} */ className="relative">
+          <div className="flex flex-col">
+            <div className="mx-auto ">
+              <Heading title="Checkout" />
             </div>
-            <div className="shadow-xl shadow-slate-200 rounded-md p-4 flex flex-col space-y-2 gap-2">
-              <div className="">
-                <p className="text-lg font-semibold text-slate-600">
-                  Payment Method
-                </p>
-              </div>
-              <hr />
 
-              <div className="flex flex-col mb-8">
-                <RadioInput
-                  id="pay-now"
-                  label="Pay now"
-                  disabled={isLoading}
-                  register={register}
-                  errors={errors}
-                  required
-                  type="radio"
-                  group="paymentMethod"
-                />
-                <div className="flex justify-between pr-4">
-                  <p className="text-sm">
-                    Complete payment with your card or a bank transfer.
-                  </p>
-                  <div className="flex gap-2">
-                    <IoCardSharp className="text-lg" />
-                    <FaCcVisa className="text-lg" />
+            <div className="grid grid-cols-1 md:grid-cols-12 gap-8 w-full">
+              <div className="col-span-1 md:col-span-7 w-full">
+                <div className="shadow-xl shadow-slate-200 rounded-md p-4 flex flex-col space-y-2 ">
+                  <div className="">
+                    <p className="text-lg font-semibold text-slate-600">
+                      Delivery Details
+                    </p>
                   </div>
-                </div>
-              </div>
-
-              <div className="flex flex-col mb-6">
-                <RadioInput
-                  id="pay-on-delivery"
-                  label="Pay on delivery"
-                  disabled={isLoading}
-                  register={register}
-                  errors={errors}
-                  required
-                  type="radio"
-                  group="paymentMethod"
-                />
-                <div className="flex justify-between pr-4 mt-1">
-                  <p className="text-sm">
-                    Pay with cash or transfer on-delivery.
+                  <hr />
+                  <p className="text-sm  text-red-500">
+                    Delivery is within Nigeria *
                   </p>
-                  <div className="flex gap-2">
-                    <BsCashCoin className="text-lg" />
-                  </div>
-                </div>
-              </div>
 
-              {/* 
+                  <Input
+                    id="streetAddress"
+                    label="Street Address"
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                  />
+                  <Input
+                    id="city"
+                    label="City / Town"
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                  />
+                  <Input
+                    id="state"
+                    label="State"
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                  />
+                  <Input
+                    id="phoneNumber"
+                    label="Phone Number"
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                  />
+                </div>
+                <div className="shadow-xl shadow-slate-200 rounded-md p-4 flex flex-col space-y-2 gap-2">
+                  <div className="">
+                    <p className="text-lg font-semibold text-slate-600">
+                      Payment Method
+                    </p>
+                  </div>
+                  <hr />
+
+                  <div className="flex flex-col mb-8">
+                    <RadioInput
+                      id="pay-now"
+                      label="Pay now"
+                      disabled={isLoading}
+                      register={register}
+                      errors={errors}
+                      required
+                      type="radio"
+                      group="paymentMethod"
+                    />
+                    <div className="flex justify-between pr-4">
+                      <p className="text-sm">
+                        Complete payment with your card or a bank transfer.
+                      </p>
+                      <div className="flex gap-2">
+                        <IoCardSharp className="text-lg" />
+                        <FaCcVisa className="text-lg" />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-col mb-6">
+                    <RadioInput
+                      id="pay-on-delivery"
+                      label="Pay on delivery"
+                      disabled={isLoading}
+                      register={register}
+                      errors={errors}
+                      required
+                      type="radio"
+                      group="paymentMethod"
+                    />
+                    <div className="flex justify-between pr-4 mt-1">
+                      <p className="text-sm">
+                        Pay with cash or transfer on-delivery.
+                      </p>
+                      <div className="flex gap-2">
+                        <BsCashCoin className="text-lg" />
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* 
               <FormField
                 control={control}
                 name="paymentMethod"
@@ -297,7 +299,7 @@ const CheckoutClient = () => {
                   />
                 )}
               /> */}
-              {/* 
+                  {/* 
               <Input
                 id="street-address"
                 label="Street Address"
@@ -314,48 +316,50 @@ const CheckoutClient = () => {
                 errors={errors}
                 required
               /> */}
+                </div>
+              </div>
+
+              <div className="col-span-1 md:col-span-5">
+                <div className="shadow-xl flex flex-col gap-6 shadow-slate-200 rounded-md p-4">
+                  <div className="">
+                    <p className="text-lg font-semibold text-slate-600">
+                      Order Summary
+                    </p>
+                  </div>
+                  <hr />
+
+                  <div className="flex justify-between">
+                    <p className="">Items Total ({cartTotalQty})</p>
+                    <p className="">{formatprice(cartTotalAmount)}</p>
+                  </div>
+                  <hr className="border-dashed" />
+                  <div className="flex justify-between font-semibold">
+                    <p>Subtotal</p>
+                    <p>{formatprice(cartTotalAmount)}</p>
+                  </div>
+
+                  <div className="flex justify-between">
+                    <p>Shipping fees</p>
+                    <p>{formatprice(0)}</p>
+                  </div>
+                  <hr />
+                  <div className="flex justify-between font-semibold text-lg">
+                    <p>Total</p>
+                    <p>{formatprice(cartTotalAmount)}</p>
+                  </div>
+                </div>
+
+                <Button
+                  label={isLoading ? "Processing..." : "Complete Order"}
+                  onClick={handleSubmit(onSubmit)}
+                ></Button>
+              </div>
             </div>
           </div>
-
-          <div className="col-span-1 md:col-span-5">
-            <div className="shadow-xl flex flex-col gap-6 shadow-slate-200 rounded-md p-4">
-              <div className="">
-                <p className="text-lg font-semibold text-slate-600">
-                  Order Summary
-                </p>
-              </div>
-              <hr />
-
-              <div className="flex justify-between">
-                <p className="">Items Total ({cartTotalQty})</p>
-                <p className="">{formatprice(cartTotalAmount)}</p>
-              </div>
-              <hr className="border-dashed" />
-              <div className="flex justify-between font-semibold">
-                <p>Subtotal</p>
-                <p>{formatprice(cartTotalAmount)}</p>
-              </div>
-
-              <div className="flex justify-between">
-                <p>Shipping fees</p>
-                <p>{formatprice(0)}</p>
-              </div>
-              <hr />
-              <div className="flex justify-between font-semibold text-lg">
-                <p>Total</p>
-                <p>{formatprice(cartTotalAmount)}</p>
-              </div>
-            </div>
-
-            <Button
-              label={isLoading ? "Processing..." : "Complete Order"}
-              onClick={handleSubmit(onSubmit)}
-            ></Button>
-          </div>
-        </div>
-      </div>
-      {isPaymentSuccessful && <PaymentSuccess />}
-    </form>
+          {isPaymentSuccessful && <PaymentSuccess />}
+        </form>
+      )}
+    </Suspense>
   );
 };
 

84c62940485dcc9efb14f9139a05cbb91382e112
Eweh Victor <ewehvictor7@gmail.com>
2024-06-17 22:29:48 +0100

    Added Add products form

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -1,5 +1,199 @@
+"use client";
+
+import Button from "@/app/components/Button";
+import Heading from "@/app/components/Heading";
+import CategoryInput from "@/app/components/inputs/CategoryInput";
+import CustomCheckBox from "@/app/components/inputs/CustomCheckBox";
+import SelectColor from "@/app/components/inputs/SelectColor";
+import TextArea from "@/app/components/inputs/TextArea";
+import Input from "@/app/components/inputs/input";
+import { categories } from "@/utils/Categories";
+import { colors } from "@/utils/Colors";
+import { useCallback, useEffect, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+export type ImageType = {
+  color: string;
+  colorCode: string;
+  image: File | null;
+};
+
+export type UploadedImageType = {
+  color: string;
+  colorCode: string;
+  image: string;
+};
+
 const AddProductForm = () => {
-  return <div></div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [images, setImages] = useState<ImageType[] | null>();
+  const [isProductCreated, setIsProductCreated] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      description: "",
+      brand: "",
+      category: "",
+      inStock: false,
+      images: [],
+      price: "",
+    },
+  });
+
+  useEffect(() => {
+    setCustomValue("images", images);
+  }, [images]);
+
+  useEffect(() => {
+    if (isProductCreated) {
+      reset();
+      setImages(null);
+      setIsProductCreated(false);
+    }
+  }, [isProductCreated]);
+
+  const onsubmit: SubmitHandler<FieldValues> = async (data) => {
+    console.log(data);
+  };
+
+  const category = watch("category");
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    });
+  };
+
+  const addImageToState = useCallback((value: ImageType) => {
+    setImages((prev) => {
+      if (!prev) {
+        return [value];
+      }
+      return [...prev, value];
+    });
+  }, []);
+
+  const removeImageFromState = useCallback((value: ImageType) => {
+    setImages((prev) => {
+      if (prev) {
+        const filteredImages = prev.filter(
+          (item) => item.color !== value.color
+        );
+
+        return filteredImages;
+      }
+
+      return prev;
+    });
+  }, []);
+
+  return (
+    <>
+      <Heading title="Add a product" />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="price"
+        label="Price"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        type="number"
+        required
+      />
+      <Input
+        id="brand"
+        label="Brand"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <TextArea
+        id="description"
+        label="Description"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+
+      <CustomCheckBox
+        id="inStock"
+        register={register}
+        label="This product is in stock"
+      />
+
+      <div className="w-full medium">
+        <div className="mb-2 font-semibold">Select a Category</div>
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-3 max-h-[50vh] overflow-y-auto">
+          {categories.map((item) => {
+            if (item.label === "All") {
+              return null;
+            }
+            return (
+              <div key={item.label} className="col-span">
+                <CategoryInput
+                  onClick={(category) => setCustomValue("category", category)}
+                  selected={category === item.label}
+                  label={item.label}
+                  icon={item.icon}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="w-full flex flex-col flex-wrap gap-4">
+        <div>
+          {" "}
+          <div className="font-bold">
+            Select the available product colors and upload their images.
+          </div>
+          <div className="text-sm">
+            You must upload an image for each of the color selected otherwise
+            your color selection will be ignored.
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-3">
+          {colors.map((item, index) => {
+            return (
+              <SelectColor
+                key={index}
+                item={item}
+                addImageToState={addImageToState}
+                removeImageFromState={removeImageFromState}
+                isProductCreated={false}
+              />
+            );
+          })}
+        </div>
+      </div>
+
+      <Button
+        label={isLoading ? "Loading..." : "Add Product"}
+        onClick={handleSubmit(onsubmit)}
+      />
+    </>
+  );
 };
 
 export default AddProductForm;
---
+++ utils/Categories.ts
@@ -0,0 +1,33 @@
+import { AiOutlineDesktop, AiOutlineLaptop } from "react-icons/ai";
+import { MdOutlineKeyboard, MdOutlinePhoneIphone, MdStorefront, MdTv, MdWatch } from "react-icons/md";
+
+export const categories = [
+    {
+        label: "All",
+        icon: MdStorefront
+    },
+    {
+        label: "Phone",
+        icon: MdOutlinePhoneIphone
+    },
+    {
+        label: "Laptop",
+        icon: AiOutlineLaptop
+    },
+    {
+        label: "Desktop",
+        icon: AiOutlineDesktop
+    },
+    {
+        label: "Watch",
+        icon: MdWatch
+    },
+    {
+        label: "TV",
+        icon: MdTv
+    },
+    {
+        label: "Accessories",
+        icon: MdOutlineKeyboard
+    },
+]
